88266e44b59805d7a1a5bcc30de0f16f1c294d6d
saileshkasaju <leosailesh@gmail.com>
2017-08-18 22:02:49 +0545

    clean code and add a simple vanilla form written with redux-form

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { Values } from 'redux-form-website-template';
+import store from './store';
+import showResults from './showResults';
+import SimpleForm from './SimpleForm';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const rootEl = document.getElementById('root');
+
+ReactDOM.render(
+  <Provider store={store}>
+    <div style={{ padding: 15 }}>
+      <h2>Simple Form</h2>
+      <SimpleForm onSubmit={showResults} />
+      <Values form="simple" />
+    </div>
+  </Provider>,
+  rootEl,
+);
